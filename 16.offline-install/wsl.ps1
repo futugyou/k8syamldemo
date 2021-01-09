a6bf3e7eaa844e4a368bb9b84e2ee2bc3765594d
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-01-09 19:09:11 +0000

    install master
    install master

--- 16.offline-install/wsl.ps1
+++ 16.offline-install/wsl.ps1
@@ -2,3 +2,7 @@ wsl --set-default-version 2
 wsl --list --verbose
 wsl --set-version Ubuntu-20.04 2
 #wsl --unregister Ubuntu-20.04
+#https://github.com/DDoSolitary/LxRunOffline/releases
+LxRunOffline.exe list
+
+#.\LxRunOffline.exe move -n <WSL name> -d <file_path>
---
+++ 17.online-install/11.master.1.20.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+hostip=$(hostname -I|cut -d" " -f 1)
+backDir=$(pwd)
+wget https://dl.k8s.io/v1.20.0/kubernetes-server-linux-amd64.tar.gz
+tar -xzvf kubernetes-server-linux-amd64.tar.gz
+cd kubernetes
+tar -xzvf  kubernetes-src.tar.gz
+cd $backDir
+
+cp -r kubernetes/server/bin/{kube-apiserver,kube-controller-manager,kube-scheduler,kubectl,kube-proxy,kubelet} /usr/local/bin/
+rm -r kubernetes-server-linux-amd64.tar.gz
+cp -f kube-apiserver.service /usr/lib/systemd/system/kube-apiserver.service
+
+cat > /etc/kubernetes/config <<EOF
+###
+# kubernetes system config
+#
+# The following values are used to configure various aspects of all
+# kubernetes services, including
+#
+#   kube-apiserver.service
+#   kube-controller-manager.service
+#   kube-scheduler.service
+#   kubelet.service
+#   kube-proxy.service
+# logging to stderr means we get it in the systemd journal
+KUBE_LOGTOSTDERR="--logtostderr=true"
+
+# journal message level, 0 is debug
+KUBE_LOG_LEVEL="--v=0"
+
+# Should this cluster be allowed to run privileged docker containers
+KUBE_ALLOW_PRIV="--allow-privileged=true"
+
+# How the controller-manager, scheduler, and proxy find the apiserver
+KUBE_MASTER="--master=http://$hostip:8080"
+EOF
+
+cat > /etc/kubernetes/apiserver <<EOF
+###
+## kubernetes system config
+##
+## The following values are used to configure the kube-apiserver
+##
+#
+## The address on the local server to listen to.
+KUBE_API_ADDRESS="--advertise-address=$hostip --bind-address=$hostip "
+#
+## The port on the local server to listen on.
+#KUBE_API_PORT="--port=8080"
+#
+## Port minions listen on
+#KUBELET_PORT="--kubelet-port=10250"
+#
+## Comma separated list of nodes in the etcd cluster
+KUBE_ETCD_SERVERS="--etcd-servers=https://$hostip:2379"
+#
+## Address range to use for services
+KUBE_SERVICE_ADDRESSES="--service-cluster-ip-range=10.254.0.0/16"
+#
+## default admission control policies
+KUBE_ADMISSION_CONTROL="--enable-admission-plugins=ServiceAccount,NamespaceLifecycle,NamespaceExists,LimitRanger,ResourceQuota"
+#
+## Add your own!
+KUBE_API_ARGS="--authorization-mode=RBAC --runtime-config=rbac.authorization.k8s.io/v1beta1 --enable-bootstrap-token-auth --token-auth-file=/etc/kubernetes/token.csv --service-node-port-range=30000-32767 --tls-cert-file=/etc/kubernetes/ssl/kubernetes.pem --tls-private-key-file=/etc/kubernetes/ssl/kubernetes-key.pem --client-ca-file=/etc/kubernetes/ssl/ca.pem --service-account-key-file=/etc/kubernetes/ssl/ca-key.pem --etcd-cafile=/etc/kubernetes/ssl/ca.pem --etcd-certfile=/etc/kubernetes/ssl/kubernetes.pem --etcd-keyfile=/etc/kubernetes/ssl/kubernetes-key.pem --apiserver-count=3 --audit-log-maxage=30 --audit-log-maxbackup=3 --audit-log-maxsize=100 --audit-log-path=/var/lib/audit.log --event-ttl=1h"
+EOF
+
+systemctl daemon-reload
+systemctl enable kube-apiserver
+systemctl start kube-apiserver
