40a9f8bd9d9255bfc6661dbd0f8e92b45f19a936
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-01-10 11:07:25 +0000

    Update 98.wsl-systemctl.sh
    close swap in wsl!!  it's very important

--- 16.offline-install/98.wsl-systemctl.sh
+++ 16.offline-install/98.wsl-systemctl.sh
@@ -5,3 +5,7 @@ apt install daemonize
 daemonize /usr/bin/unshare --fork --pid --mount-proc /lib/systemd/systemd --system-unit=basic.target
 echo 'exec'
 exec sudo  nsenter -t $(pidof systemd) -a su - $LOGNAME
+
+#####  about close swap 
+#####  look https://docs.microsoft.com/en-us/windows/wsl/release-notes ,
+#####  search "%UserProfile%\.wslconfig", then you will know how to write this is .wslconfig
