b5b9812f0dc92680f1dfbb97abc4a40e41cbde02
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-01-10 11:17:22 +0000

    Update wsl.ps1
    fixed linux ip in wsl

--- 16.offline-install/wsl.ps1
+++ 16.offline-install/wsl.ps1
@@ -4,5 +4,12 @@ wsl --set-version Ubuntu-20.04 2
 #wsl --unregister Ubuntu-20.04
 #https://github.com/DDoSolitary/LxRunOffline/releases
 LxRunOffline.exe list
+# wsl --shutdown Ubuntu-20.04
+# LxRunOffline.exe move -n Ubuntu-20.04 -d E:\wsl
 
-#.\LxRunOffline.exe move -n <WSL name> -d <file_path>
+/etc/wsl.conf
+
+[network]
+generateHosts = false
+#fixed ip
+sudo vi /etc/hosts
