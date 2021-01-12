9a159c8dd549d6ed5b522fbd0b378d85a1995d22
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-01-12 12:10:39 +0000

    add dashboard
    add dashboard

--- 18.kubeadm/1.kubeadm.sh
+++ 18.kubeadm/1.kubeadm.sh
@@ -26,7 +26,7 @@ sudo apt-get update
 sudo apt-get install -y kubelet kubeadm kubectl
 sudo apt-mark hold kubelet kubeadm kubectl 
 
-kubeadm init --pod-network-cidr=10.244.0.0/16  --apiserver-advertise-address=$IPorFQDN  
+kubeadm init --pod-network-cidr=10.244.0.0/16  --apiserver-advertise-address=$IPorFQDN  #--apiserver-cert-extra-sans="192.168.31.146" if use wsl2 , you have set this win host ip 
 #------if 509 do this
 # sudo apt-get update   !
 # sudo apt-get upgrade  !
@@ -43,3 +43,6 @@ kubeadm init --pod-network-cidr=10.244.0.0/16  --apiserver-advertise-address=$IP
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
+
+# exec this ,then you can use master as work node
+# kubectl taint node {nodename} node-role.kubernetes.io/master:NoSchedule-
---
+++ 20.kind/03.dashboard.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+# dashboard https://github.com/kubernetes/dashboard
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.1.0/aio/deploy/recommended.yaml
+
+#https://github.com/kubernetes/dashboard/blob/master/docs/user/access-control/creating-sample-user.md
+
+cat <<EOF | kubectl apply -f -
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: admin-user
+  namespace: kubernetes-dashboard
+EOF
+
+cat <<EOF | kubectl apply -f -
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRoleBinding
+metadata:
+  name: admin-user
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+- kind: ServiceAccount
+  name: admin-user
+  namespace: kubernetes-dashboard
+EOF
+
+kubectl -n kubernetes-dashboard describe secret $(kubectl -n kubernetes-dashboard get secret | grep admin-user | awk '{print $1}')
+
+# then exec kubectl proxy ,
+# and open http://localhost:8001/api/v1/namespaces/kubernetes-dashboard/services/https:kubernetes-dashboard:/proxy/
