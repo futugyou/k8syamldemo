a2870feba8e18d568d2b88ffb01d07f74294a94a
futugyousuzu <17871902+futugyu@users.noreply.github.com>
2021-01-13 11:38:31 +0000

    add kubectl
    add kubectl...

--- 21.kops/0.instal-kops.sh
+++ 21.kops/0.instal-kops.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 # https://github.com/kubernetes/kops/releases
-wget https://github.com/kubernetes/kops/releases/download/v1.18.2/kops-linux-amd64
-chmod +x kops-linux-amd64
-mv kops-linux-amd64 /usr/local/bin/kops
+# wget https://github.com/kubernetes/kops/releases/download/v1.18.2/kops-linux-amd64
+# chmod +x kops-linux-amd64
+# mv kops-linux-amd64 /usr/local/bin/kops
+curl -Lo kops https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+chmod +x kops
+sudo mv kops /usr/local/bin/kops
