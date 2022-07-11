cab6336ec813bfb7e2a65dd868ea50fdf439deda
futugyou <17871902+futugyou@users.noreply.github.com>
2022-07-11 17:56:20 +0800

    add eks demo to terraform
    first , create a new vpc

---
+++ 06.terraform/aws/eks/variables.tf
@@ -0,0 +1,52 @@
+variable "default_region" {
+  description = "default region"
+  type        = string
+  default     = "ap-southeast-1"
+}
+
+variable "cluster_name" {
+  description = "Name of Cluster"
+  type        = string
+  default     = "eks-osim-dev"
+}
+
+variable "cluster_version" {
+  description = "Version of Cluster"
+  type        = string
+  default     = "1.22"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+data "aws_availability_zones" "available" {}
+
+variable "vpc_private_subnets" {
+  description = "Private subnets for VPC"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+}
+
+variable "vpc_public_subnets" {
+  description = "Public subnets for VPC"
+  type        = list(string)
+  default     = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+}
+
+variable "vpc_enable_nat_gateway" {
+  description = "Enable NAT gateway for VPC"
+  type        = bool
+  default     = true
+}
+
+variable "vpc_tags" {
+  description = "Tags to apply to resources created by VPC module"
+  type        = map(string)
+  default = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
