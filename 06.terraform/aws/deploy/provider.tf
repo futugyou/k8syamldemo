146753bfe073bb7cfba2933615f03cea26e42232
futugyou <17871902+futugyou@users.noreply.github.com>
2022-07-18 13:03:16 +0800

    move k8s provider to provider.tf
    move k8s provider to provider.tf

--- 06.terraform/aws/deploy/provider.tf
+++ 06.terraform/aws/deploy/provider.tf
@@ -1,5 +1,9 @@
 terraform {
   required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.0.1"
+    }
     aws = {
       source  = "hashicorp/aws"
       version = ">= 3.20.0"
@@ -12,6 +16,32 @@ terraform {
   }
 }
 
+data "aws_eks_cluster" "cluster" {
+  name = var.cluster_name
+}
+
 provider "aws" {
   region = var.region
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1"
+    args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+    command     = "aws"
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
+      command     = "aws"
+    }
+  }
 }
