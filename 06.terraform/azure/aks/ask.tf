3a7f194bb61debab2db9756314b34fa4b71117c8
futugyou <17871902+futugyou@users.noreply.github.com>
2022-08-11 10:59:47 +0800

    add azure aks terraform
    add azure aks terraform

---
+++ 06.terraform/azure/aks/ask.tf
@@ -0,0 +1,25 @@
+resource "azurerm_resource_group" "resource_group" {
+  name     = "example-resources"
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "cluster" {
+  name                = var.cluster_name
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  dns_prefix          = "exampleaks1"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
---
+++ 06.terraform/azure/aks/variables.tf
@@ -0,0 +1,11 @@
+variable "location" {
+  description = "default location"
+  type        = string
+  default     = "Southeast Asia"
+}
+
+variable "cluster_name" {
+  description = "Name of Cluster"
+  type        = string
+  default     = "eks-osim-dev"
+}
