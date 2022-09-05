3c2d9ac2ca0765c1bb1ee731392650aa754c61ef
futugyou <17871902+futugyou@users.noreply.github.com>
2022-09-05 14:17:00 +0800

    CNP-16 fix s3 warning
    CNP-16 fix s3 warning

--- 06.terraform/aws/terraform-eks-code/01.tf-setup/s3-bucket.tf
+++ 06.terraform/aws/terraform-eks-code/01.tf-setup/s3-bucket.tf
@@ -1,15 +1,20 @@
 resource "aws_s3_bucket" "terraform_state" {
   bucket = var.s3_bucket_name
+}
 
-  versioning {
-    enabled = true
-  }
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.bucket
 
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        sse_algorithm = "AES256"
-      }
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
     }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
   }
 }
