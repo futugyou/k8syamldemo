94658755bb088e0471067289461c14b843676b99
futugyou <17871902+futugyou@users.noreply.github.com>
2022-09-06 10:04:57 +0800

    CNP-17 add eks route table data
    CNP-17 add eks route table data

--- 06.terraform/aws/terraform-eks-code/04.link-net/03.data-route-table.tf
+++ 06.terraform/aws/terraform-eks-code/04.link-net/03.data-route-table.tf
@@ -5,3 +5,20 @@ data "aws_route_table" "cicd-rtb" {
     values = ["cicd-private-route-table"]
   }
 }
+
+data "aws_route_table" "isolated" {
+  vpc_id = data.aws_vpc.vpc-eks.id
+  filter {
+    name   = "tag:Name"
+    values = [format("%s-cluster/IsolatedRouteTable", var.cluster-vpc-name)]
+  }
+}
+
+data "aws_route_tables" "private-rts" {
+  vpc_id = data.aws_vpc.vpc-eks.id
+
+  filter {
+    name   = "tag:Name"
+    values = ["*-cluster/PrivateRouteTable*"]
+  }
+}
