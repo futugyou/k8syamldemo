9aee9b724a416d2b968847c054c33aa5bacf4c6b
futugyou <17871902+futugyou@users.noreply.github.com>
2022-09-06 10:03:46 +0800

    CNP-17 add eks security group, boss control node and work node
    CNP-17 add eks security group, boss control node and work node

--- 06.terraform/aws/terraform-eks-code/04.link-net/02.data-security-group.tf
+++ 06.terraform/aws/terraform-eks-code/04.link-net/02.data-security-group.tf
@@ -20,3 +20,19 @@ data "aws_security_group" "cloud9-sg" {
 data "aws_iam_instance_profile" "cloud9-ec2-profile" {
   name = data.aws_instance.cloud9-ec2.iam_instance_profile
 }
+
+data "aws_security_group" "cluster-sg" {
+  vpc_id = data.aws_vpc.vpc-eks.id
+  filter {
+    name   = "tag:Name"
+    values = [format("%s-cluster/ControlPlaneSecurityGroup", var.cluster-vpc-name)]
+  }
+}
+
+data "aws_security_group" "allnodes-sg" {
+  vpc_id = data.aws_vpc.vpc-eks.id
+  filter {
+    name   = "tag:Name"
+    values = [format("%s-cluster/ClusterSharedNodeSecurityGroup", var.cluster-vpc-name)]
+  }
+}
