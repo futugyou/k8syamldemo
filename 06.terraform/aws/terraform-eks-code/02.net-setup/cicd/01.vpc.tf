bd1d9eb14776164d228f138b781f964dc5ba241a
futugyou <17871902+futugyou@users.noreply.github.com>
2022-09-07 16:09:46 +0800

    CNP-19 fix warning
    CNP-19 fix warning

--- 06.terraform/aws/terraform-eks-code/02.net-setup/cicd/01.vpc.tf
+++ 06.terraform/aws/terraform-eks-code/02.net-setup/cicd/01.vpc.tf
@@ -1,8 +1,6 @@
 resource "aws_vpc" "vpc-cicd" {
   assign_generated_ipv6_cidr_block = false
   cidr_block                       = var.cicd-vpc-cidr
-  enable_classiclink               = false
-  enable_classiclink_dns_support   = false
   enable_dns_hostnames             = false
   enable_dns_support               = true
   instance_tenancy                 = "default"
--- 06.terraform/aws/terraform-eks-code/02.net-setup/cluster/01.vpc.tf
+++ 06.terraform/aws/terraform-eks-code/02.net-setup/cluster/01.vpc.tf
@@ -3,8 +3,6 @@
 resource "aws_vpc" "cluster" {
   assign_generated_ipv6_cidr_block = false
   cidr_block                       = var.cluster-vpc-cidr
-  enable_classiclink               = false
-  enable_classiclink_dns_support   = false
   enable_dns_hostnames             = true
   enable_dns_support               = true
   instance_tenancy                 = "default"
