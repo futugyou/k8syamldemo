6ccdc66c866cbc84d8aa3f638ed733cbea5aaae9
futugyou <17871902+futugyou@users.noreply.github.com>
2022-09-08 12:29:16 +0800

    CNP-20 add datas and locals
    CNP-20 add datas and locals

---
+++ 06.terraform/aws/terraform-eks-code/06.eks-node/variables.tf
@@ -0,0 +1,25 @@
+variable "default_region" {
+  description = "cluster region"
+  type        = string
+  default     = "ap-southeast-1"
+}
+
+variable "cluster-name" {
+  description = "cluster name"
+  type        = string
+  default     = "terraform-eks"
+}
+
+
+variable "node-group-name" {
+  description = "node group name"
+  type        = string
+  default     = "node-one"
+}
+
+
+variable "cluster_version" {
+  description = "Version of Cluster"
+  type        = string
+  default     = "1.23"
+}
