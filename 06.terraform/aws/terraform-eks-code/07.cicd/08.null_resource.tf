2a598c72df453d92bd3a42cf22c323ea1b5793c6
futugyou <17871902+futugyou@users.noreply.github.com>
2022-09-09 16:34:54 +0800

    CNP-21 add null_resource to test pipeline
    CNP-21 add null_resource to test pipeline

---
+++ 06.terraform/aws/terraform-eks-code/07.cicd/08.null_resource.tf
@@ -0,0 +1,39 @@
+resource "null_resource" "load_ecr" {
+  triggers = {
+    always_run = timestamp()
+  }
+  depends_on = [aws_ecr_repository.busybox]
+  provisioner "local-exec" {
+    on_failure  = fail
+    when        = create
+    interpreter = ["/bin/bash", "-c"]
+    command     = <<EOT
+        ./load_ecr.sh ${var.karpenter_version}
+        #echo "Karpenter ..."
+        #docker pull public.ecr.aws/karpenter/controller:v${var.karpenter_version}
+        #docker tag public.ecr.aws/karpenter/controller:v${var.karpenter_version} $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/karpenter/controller:v${var.karpenter_version}
+        #docker pull public.ecr.aws/karpenter/webhook:v${var.karpenter_version}
+        #docker tag public.ecr.aws/karpenter/webhook:v${var.karpenter_version} $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/karpenter/webhook:v${var.karpenter_version}
+        #docker push $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/karpenter/webhook:v${var.karpenter_version}
+        #docker push $ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/karpenter/controller:v${var.karpenter_version}
+        echo "************************************************************************************"
+     EOT
+  }
+}
+
+resource "null_resource" "auth-cidr" {
+  triggers = {
+    always_run = timestamp()
+  }
+  depends_on = [aws_iam_role.codebuild-eks-cicd-build-app-service-role]
+  provisioner "local-exec" {
+    on_failure  = fail
+    when        = create
+    interpreter = ["/bin/bash", "-c"]
+    command     = <<EOT
+        echo "auth cicd role for K8s"
+        ./auth-cicd.sh
+        echo "************************************************************************************"
+     EOT
+  }
+}
