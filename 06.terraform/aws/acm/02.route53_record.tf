218424e16a270349f5494a8af29ec651992ad0ec
futugyou <17871902+futugyou@users.noreply.github.com>
2022-09-16 10:52:27 +0800

    CNP-30 add route53
    CNP-30 add route53

---
+++ 06.terraform/aws/acm/02.route53_record.tf
@@ -0,0 +1,20 @@
+resource "aws_route53_zone" "route53_zone" {
+  name = var.domain_name
+}
+
+resource "aws_route53_record" "route53_record" {
+  for_each = {
+    for dvo in aws_acm_certificate.acm_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.route53_zone.zone_id
+}
