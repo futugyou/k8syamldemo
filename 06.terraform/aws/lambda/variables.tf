a9e1dcd02c67c48f827d1727db64b4ddfc9428ab
futugyou <17871902+futugyou@users.noreply.github.com>
2022-09-17 19:03:53 +0800

    CNP-31 add lambda iam
    CNP-31 add lambda iam

---
+++ 06.terraform/aws/lambda/variables.tf
@@ -0,0 +1,11 @@
+variable "default_region" {
+  description = "cluster region"
+  type        = string
+  default     = "ap-southeast-1"
+}
+
+variable "lambda_iam_role" {
+  description = "lambda iam role"
+  type        = string
+  default     = "iam_for_lambda"
+}
