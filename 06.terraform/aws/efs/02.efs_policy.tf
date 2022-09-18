c58392e3912067ef776d601403ff22bfba6975b1
futugyou <17871902+futugyou@users.noreply.github.com>
2022-09-18 16:50:23 +0800

    CNP-32 add efs file system policy
    CNP-32 add efs file system policy

---
+++ 06.terraform/aws/efs/02.efs_policy.tf
@@ -0,0 +1,31 @@
+resource "aws_efs_file_system_policy" "policy" {
+  file_system_id = aws_efs_file_system.file_system.id
+
+  bypass_policy_lockout_safety_check = true
+
+  policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Id": "ExamplePolicy01",
+    "Statement": [
+        {
+            "Sid": "ExampleStatement01",
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "*"
+            },
+            "Resource": "${aws_efs_file_system.file_system.arn}",
+            "Action": [
+                "elasticfilesystem:ClientMount",
+                "elasticfilesystem:ClientWrite"
+            ],
+            "Condition": {
+                "Bool": {
+                    "aws:SecureTransport": "true"
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
