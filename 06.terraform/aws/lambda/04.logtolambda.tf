aabfdf3c83ba9b1b494afc0b1bc440e84d3d9c53
futugyou <17871902+futugyou@users.noreply.github.com>
2022-11-07 13:35:04 +0800

    update lambda to add aws_lambda_permission
    it will fix the lambda policy

--- 06.terraform/aws/lambda/04.logtolambda.tf
+++ 06.terraform/aws/lambda/04.logtolambda.tf
@@ -21,8 +21,19 @@ resource "aws_lambda_function" "log_group_lambda" {
 
 resource "aws_cloudwatch_log_subscription_filter" "demo_logfilter" {
   name            = "demo_logfilter"
+  depends_on = [aws_lambda_permission.allow_cloudwatch]
   log_group_name  = aws_cloudwatch_log_group.log_group_for_lambda.name
   filter_pattern  = "{ $.LogLevel = \"Error\" }"
   destination_arn = aws_lambda_function.log_group_lambda.arn
 }
 
+data "aws_caller_identity" "current" {}
+
+resource "aws_lambda_permission" "allow_cloudwatch" {
+  statement_id   = "AllowExecutionFromCloudWatch_0"
+  action         = "lambda:InvokeFunction"
+  function_name  = aws_lambda_function.log_group_lambda.function_name
+  source_account = data.aws_caller_identity.current.account_id
+  principal      = "logs.amazonaws.com"
+  source_arn     = "${aws_cloudwatch_log_group.log_group_for_lambda.arn}:*"
+}
