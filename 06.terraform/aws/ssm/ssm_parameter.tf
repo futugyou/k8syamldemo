95c33148a38935699a266f57501e363626ef3e66
futugyou <17871902+futugyou@users.noreply.github.com>
2022-11-10 10:01:32 +0800

    add readjson / readyaml demo for ssm
    add readjson / readyaml demo for ssm

--- 06.terraform/aws/ssm/ssm_parameter.tf
+++ 06.terraform/aws/ssm/ssm_parameter.tf
@@ -1,3 +1,10 @@
+locals {
+  // this is for jsondecode/jsonencode demo, in product those two function can omit
+  json_data = jsondecode(file("${path.module}/json-values.json"))
+  // this is for yamldecode/yamlencode demo, in product those two function can omit
+  yaml_data = yamldecode(file("${path.module}/yaml-values.yaml"))
+}
+
 resource "aws_ssm_parameter" "AppSetting-Session" {
   data_type = "text"
   key_id    = "alias/aws/ssm"
@@ -5,4 +12,23 @@ resource "aws_ssm_parameter" "AppSetting-Session" {
   tier      = "Standard"
   type      = "SecureString"
   value     = "{\n\"ExpiryTime\":86400\n}"
+}
+
+resource "aws_ssm_parameter" "read-json" {
+  data_type = "text"
+  key_id    = "alias/aws/ssm"
+  name      = "/AppSetting/ReadJson"
+  tier      = "Standard"
+  type      = "SecureString"
+  value     = jsonencode(local.json_data)
+}
+
+
+resource "aws_ssm_parameter" "read-yaml" {
+  data_type = "text"
+  key_id    = "alias/aws/ssm"
+  name      = "/AppSetting/ReadYaml"
+  tier      = "Standard"
+  type      = "SecureString"
+  value     = yamlencode(local.yaml_data)
 }
