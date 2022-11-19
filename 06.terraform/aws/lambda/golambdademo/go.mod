3659889ffe7c01ef102e8ceace5d2d0d9f482347
futugyou <17871902+futugyou@users.noreply.github.com>
2022-11-19 15:39:07 +0800

    create sql server database
    create sql server database

--- 06.terraform/aws/lambda/golambdademo/go.mod
+++ 06.terraform/aws/lambda/golambdademo/go.mod
@@ -2,4 +2,10 @@ module github.com/futugyou/golambdademo
 
 go 1.19
 
-require github.com/aws/aws-lambda-go v1.34.1 // indirect
+require (
+	github.com/aws/aws-lambda-go v1.34.1 // indirect
+	github.com/denisenkom/go-mssqldb v0.12.3 // indirect
+	github.com/golang-sql/civil v0.0.0-20190719163853-cb61b32ac6fe // indirect
+	github.com/golang-sql/sqlexp v0.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20220622213112-05595931fe9d // indirect
+)
--- 06.terraform/aws/lambda/golambdademo/main.go
+++ 06.terraform/aws/lambda/golambdademo/main.go
@@ -3,16 +3,48 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
+
+	"database/sql"
 
 	"github.com/aws/aws-lambda-go/lambda"
+
+	_ "github.com/denisenkom/go-mssqldb"
 )
 
-type MyEvent struct {
-	Name string `json:"name"`
+type SqlEvent struct {
+	Server   string `json:"server"`
+	User     string `json:"user"`
+	Password string `json:"password"`
+	Port     int    `json:"port"`
 }
 
-func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
-	return fmt.Sprintf("Hello %s!", name.Name), nil
+func HandleRequest(ctx context.Context, ev SqlEvent) (string, error) {
+	connString := fmt.Sprintf("server=%s;user id=%s;password=%s;port=%d",
+		ev.Server,
+		ev.User,
+		ev.Password,
+		ev.Port)
+
+	conn, err := sql.Open("mssql", connString)
+
+	if err != nil {
+		log.Fatal("open connection failed:", err.Error())
+		return "Open connection failed", err
+	}
+
+	defer conn.Close()
+
+	_, err = conn.Exec("USE master \n" +
+		"IF NOT EXISTS (SELECT [name] FROM sys.databases WHERE [name] = N'EventBus1') \n" +
+		"CREATE DATABASE EventBus1 \n")
+
+	if err != nil {
+		log.Fatal("exec failed:", err.Error())
+		return "exec failed", err
+	}
+
+	return "ok", nil
 }
 
 func main() {
